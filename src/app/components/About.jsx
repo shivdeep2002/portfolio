f237426826da3bbf78ad9545489e68aaea31487a
Shivdeep Mishra <107456851+shivdeep2002@users.noreply.github.com>
2025-01-09 16:39:08 +0530

    Changes to be committed: 	modified:   src/app/components/About.jsx

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -11,14 +11,18 @@ const About = () => {
             alt="hero"
             width={1000}
             height={1000}
-            className="h-[500px]  border-8 border-blue-600 bg-black"
+            className="h-[600px]  border-8 border-blue-900  bg-gray-500"
           />
         </div>
         <div className="w-1/2">
           <div>
             <div className="flex flex-col gap-3">
-              <h1 className="text-4xl font-bold">About <span className="text-blue-600">Me</span></h1>
-              <h2 className="text-2xl font-bold">Full Stack Developer Developer</h2>
+              <h1 className="text-4xl font-bold">
+                About <span className="text-blue-600">Me</span>
+              </h1>
+              <h2 className="text-2xl font-bold">
+                Full Stack Developer Developer
+              </h2>
               <p>
                 Hello, I am Naveen singh. I am in my 3rd year at Makhanlal
                 University. I am currently studying BCA. I am a passionate and
@@ -28,7 +32,9 @@ const About = () => {
                 set.
               </p>
               <div className="mt-5">
-              <button className=" shadow-blue-600 rounded-lg px-8 bg-blue-600 py-3 ">Read More</button>
+                <button className=" shadow-blue-600 rounded-lg px-8 bg-blue-600 py-3 ">
+                  Read More
+                </button>
               </div>
             </div>
           </div>
